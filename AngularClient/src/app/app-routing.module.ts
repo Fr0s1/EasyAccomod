de58b449443af964f56ca17b09d5678e6bfa3c1b
frost <dthieu223@gmail.com>
2020-11-10 10:10:46 +0700

    test

--- AngularClient/src/app/app-routing.module.ts
+++ AngularClient/src/app/app-routing.module.ts
@@ -4,14 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LogInComponent } from './log-in/log-in.component'
 import { AccountComponent } from './admin/verify/accounts/accounts.component';
 import { PostsComponent } from './admin/verify/posts/posts.component';
-import { HomeComponent } from './admin/home/home.component';
-
+import { AdminComponent } from './admin/admin.component'
 // Tạo đường dẫn để duyệt tài khoản/bài đăng cho admin với định dạng /admin/verify/...
 
 const routes: Routes = [
-  { path: 'admin/verify/accounts', component: AccountComponent },
-  { path: 'admin/verify/posts', component: PostsComponent },
-  { path: 'admin', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
+  // { path: 'admin/verify/accounts', component: AccountComponent },
+  // { path: 'admin/verify/posts', component: PostsComponent },
   { path: 'login', component: LogInComponent }
 ];
 
