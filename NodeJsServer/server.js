11db4e911b8efc64f00814e25ab37b3c6626db94
frost <dthieu223@gmail.com>
2020-11-11 22:06:56 +0700

    nothing

--- NodeJsServer/server.js
+++ NodeJsServer/server.js
@@ -1,20 +1,10 @@
 const express = require("express");
-const path = require('path');
-const room = require("./db/room/room");
-const roomRouter = require('./router/room')
-const userRouter = require('./router/user')
+
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.json())
 
-const roomImage = path.join(__dirname, '/db/room-images/')
-app.use(roomRouter)
-app.use(userRouter)
-app.use(express.static(roomImage))
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
-
-console.log(roomImage)
