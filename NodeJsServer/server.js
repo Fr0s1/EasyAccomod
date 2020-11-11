4383ef9ea739859be5f516d9af4e9809cda98513
frost <dthieu223@gmail.com>
2020-11-12 00:12:11 +0700

    Tao form cho bai dang

--- NodeJsServer/server.js
+++ NodeJsServer/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 
-
 const PORT = process.env.PORT || 8080;
 const app = express();
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
