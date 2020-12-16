0b3164bf9da63fa0642e7cdcafc3a88d714889e7
frost <dthieu223@gmail.com>
2020-12-16 18:50:26 +0700

    Update CSS for rating (not finished)

--- AngularClient/src/app/comments/comments.component.ts
+++ AngularClient/src/app/comments/comments.component.ts
@@ -37,6 +37,7 @@ export class CommentsComponent implements OnInit {
   ngAfterViewInit() {
     console.log(this.rating.nativeElement.children)
   }
+  
   postID
   postInfo
 
@@ -94,7 +95,7 @@ export class CommentsComponent implements OnInit {
     let id: number = event.target.id
 
     for (let i = 1; i <= id; i++) {
-      this.rating.nativeElement.children[i].className += ' checked';
+      this.rating.nativeElement.children[5-i+1].className = 'fa fa-star checked';
     }
   }
 }
