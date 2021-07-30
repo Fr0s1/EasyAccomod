c9ab367bd7b5e16bf2865bcaf89cab9b118d3883
Hieu <dthieu223@gmail.com>
2021-07-30 14:00:09 +0700

    Fetch content by https

--- AngularClient/src/app/_helpers/backend.ts
+++ AngularClient/src/app/_helpers/backend.ts
@@ -1,6 +1,6 @@
 const Backend = {
-    url: 'http://ea-nlb-db977c75f15d7cdd.elb.ap-southeast-1.amazonaws.com:8081',
-    chat_server: 'http://ea-nlb-db977c75f15d7cdd.elb.ap-southeast-1.amazonaws.com:3000'    
+    url: 'https://ea-nlb-db977c75f15d7cdd.elb.ap-southeast-1.amazonaws.com:8081',
+    chat_server: 'https://ea-nlb-db977c75f15d7cdd.elb.ap-southeast-1.amazonaws.com:3000'    
 }
 
 export { Backend }
