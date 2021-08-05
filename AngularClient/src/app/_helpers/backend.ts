cc1a8a6c622a00f59e949898a8c0a09ccddd6d09
Hieu <dthieu223@gmail.com>
2021-08-05 11:22:37 +0700

    Update lb endpoint

--- AngularClient/src/app/_helpers/backend.ts
+++ AngularClient/src/app/_helpers/backend.ts
@@ -1,6 +1,6 @@
 const Backend = {
-    url: 'https://frost-ea-nlb-abc776827dbda1b0.elb.ap-southeast-1.amazonaws.com:8081',
-    chat_server: 'https://frost-ea-nlb-abc776827dbda1b0.elb.ap-southeast-1.amazonaws.com:3000'    
+    url: 'https://frost-ea-nlb-7adee4ccda114d90.elb.ap-southeast-1.amazonaws.com:8081',
+    chat_server: 'https://frost-ea-nlb-7adee4ccda114d90.elb.ap-southeast-1.amazonaws.com:3000'    
 }
 
 export { Backend }
