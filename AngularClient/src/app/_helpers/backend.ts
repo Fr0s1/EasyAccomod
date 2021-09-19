c3268494e6f20ee25c852e19fbbe8c4d0fd1f59c
Hieu <dthieu223@gmail.com>
2021-09-19 20:37:41 +0700

    Add docker compose file for local run

--- AngularClient/src/app/_helpers/backend.ts
+++ AngularClient/src/app/_helpers/backend.ts
@@ -1,6 +1,6 @@
 const Backend = {
-    url: 'https://api.frostdevops.com:8080',
-    chat_server: 'https://api.frostdevops.com:3000'    
+    url: 'http://localhost:8080',
+    chat_server: 'http://localhost:3000'    
 }
 
 export { Backend }
