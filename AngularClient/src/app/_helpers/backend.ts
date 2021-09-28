1d2ebf0bb331a165aaf5d8c003d4a28af9f17fe6
Hieu <dthieu223@gmail.com>
2021-09-28 21:36:30 +0700

    change backend url

--- AngularClient/src/app/_helpers/backend.ts
+++ AngularClient/src/app/_helpers/backend.ts
@@ -1,7 +1,8 @@
+const basePath = 'localhost'
 const Backend = {
-    url: 'http://k8s-default-eaingres-6a2c4e21ee-166822039.ap-southeast-1.elb.amazonaws.com/api/backend',
-    chat_server: 'http://k8s-default-eaingres-6a2c4e21ee-166822039.ap-southeast-1.elb.amazonaws.com/api/chat',
-    socketio_endpoint: 'ws://k8s-default-eaingres-6a2c4e21ee-166822039.ap-southeast-1.elb.amazonaws.com/api/chat'
+    url: `http://${basePath}/api/backend`,
+    chat_server: `http://${basePath}/api/chat`,
+    socketio_endpoint: `ws://${basePath}/api/chat`
 }
 
 export { Backend }
